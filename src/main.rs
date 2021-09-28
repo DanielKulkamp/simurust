6a2c49112e818a472530fae024ee8a0fb532ddc9
DanielKulkamp <daniel.kulkamp@gmail.com>
2021-09-28 19:38:06 -0300

    rodando

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ impl Team {
             Ordering::Greater => {
                 self.wins += 1;
                 self.home_wins += 1;
-                self.points += 1;
+                self.points += 3;
             }
         }
     }
@@ -89,7 +89,7 @@ impl Team {
             Ordering::Greater => {
                 self.wins += 1;
                 self.away_wins += 1;
-                self.points += 1;
+                self.points += 3;
             }
         }
     }
@@ -158,42 +158,72 @@ impl Team {
 }
 
 fn main() {
+    let algoritmo = Team::sim_match_rand;
+    let n_simulacoes = 100000u32;
+
     let conteudo = fs::read_to_string("C:\\Users\\danie\\Simulador Serie B\\SimuladorPontosCorridos\\TABELA SERIE B.txt").expect("Erro");
-    let team_names = vec!["CFC", "BOT", "AVA", "CRB", "GOI", "GUA", "CSA", "VAS", "REM", "SAM", "PON", "OPE", "NAU", "CRU", "VIL", "LON",
+    let mut team_names = vec!["CFC", "BOT", "AVA", "CRB", "GOI", "GUA", "CSA", "VAS", "REM", "SAM", "PON", "OPE", "NAU", "CRU", "VIL", "LON",
         "VIT", "BRU", "CON", "BRA"];
     
-        let mut teams_map  :  HashMap<&str, Team> = HashMap::new();    
+    let mut promotion_map : HashMap<&str, i32> = HashMap::new();
+    let mut title_map : HashMap<&str, i32> = HashMap::new();
+    let mut relegation_map : HashMap<&str, i32> = HashMap::new();
+
     for t in team_names.iter() {
-        teams_map.insert(t, Team::create(t.to_string()) );
+        promotion_map.insert(t, 0);
+        title_map.insert(t, 0);
+        relegation_map.insert(t, 0);
     }
+
+    let mut title_points : HashMap<i32, i32> = HashMap::new();
+    let mut promotion_points : HashMap<i32, i32> = HashMap::new();
+    let mut relegation_points : HashMap<i32, i32> = HashMap::new();
+
+    for i in 0..100 {
+        title_points.insert(i, 0);
+        promotion_points.insert(i, 0);
+        relegation_points.insert(i, 0);
+    }
+
+
     
-    for line in conteudo.lines(){
-        let v : Vec<&str> = line.split(' ').collect();
-        let home : &Team;
-        let away : &Team;
-        home = match teams_map.get(v[0]){
-            Some(team) => team,
-            None => {
-                panic!("erro time não existe");
-            }
-        };
-                    
-        away = match teams_map.get(v[2]){
-            Some(team) => team,
-            None => {
-                panic!("erro time não existe");
-            }
-        };
-        
-        if v[1] != "-" {
+    for i in 0..n_simulacoes {
+        let mut teams_map  :  HashMap<&str, Team> = HashMap::new();    
+        for t in team_names.iter() {
+            teams_map.insert(t, Team::create(t.to_string()) );
+        }
+
+        for line in conteudo.lines(){
+            let v : Vec<&str> = line.split(' ').collect();
+            let home : &Team;
+            let away : &Team;
+            home = match teams_map.get(v[0]) {
+                Some(team) => team,
+                None => {
+                    panic!("erro time não existe");
+                }
+            };
+                        
+            away = match teams_map.get(v[2]){
+                Some(team) => team,
+                None => {
+                    panic!("erro time não existe");
+                }
+            };
             let home_score : i32;
             let away_score : i32;
-            let mut iter = v[1].chars();
-
-            home_score = iter.next().unwrap().to_digit(10u32).unwrap() as i32;
-            println!("Home score: {}", home_score);
-            iter.next();
-            away_score = iter.next().unwrap().to_digit(10u32).unwrap() as i32;
+            if v[1] != "-" {
+                let mut iter = v[1].chars();
+                home_score = iter.next().unwrap().to_digit(10u32).unwrap() as i32;
+                iter.next();
+                away_score = iter.next().unwrap().to_digit(10u32).unwrap() as i32;
+            } else {
+                
+                let (h_s, a_s) = algoritmo(home, away);
+                home_score = h_s;
+                away_score = a_s;
+                
+            }
 
             let w : f32 = match home_score.cmp(&away_score) {
                 Ordering::Greater => 1.0f32,
@@ -206,25 +236,47 @@ fn main() {
             {
                 let homet : &mut Team = teams_map.get_mut(v[0]).unwrap();
                 homet.home_match(home_score, away_score, delta_rating);
-                println!("{:?}", homet);
-                
+                             
             }
 
             {
                 let awayt : &mut Team = teams_map.get_mut(v[2]).unwrap();
                 awayt.away_match(home_score, away_score, delta_rating);
-                println!("{:?}", awayt);
-
+                
             }
 
+           
+            
+        }
+        team_names.sort_by_key(|a| -1*teams_map.get(a).unwrap().goal_difference);
+        team_names.sort_by_key(|a| -1*teams_map.get(a).unwrap().wins);
+        team_names.sort_by_key(|a| -1*teams_map.get(a).unwrap().points);
+
+        for t in team_names[0..4].iter(){
+            *promotion_map.entry(t).or_insert(0) += 1;            
+        }
+        *title_map.entry(team_names[0]).or_insert(0) +=1;
+        for t in team_names[16..20].iter(){
+            *relegation_map.entry(t).or_insert(0) += 1;            
+        }
+        for i in teams_map.get(team_names[3]).unwrap().points..100 {
+            *promotion_points.entry(i).or_insert(0) += 1;
+        }
+        for i in teams_map.get(team_names[0]).unwrap().points..100 {
+            *title_points.entry(i).or_insert(0) += 1;
         }
 
-        //onana
-        
-    
     }
+    team_names.sort_by_key(|a| relegation_map.get(a).unwrap());
+    team_names.sort_by_key(|a| promotion_map.get(a).unwrap()*-1);
+    team_names.sort_by_key(|a| title_map.get(a).unwrap()*-1);
 
-    
+    for team in team_names.iter() {
+        let title : f32 = 100.0 * (*title_map.get(team).unwrap() as f32 )/ (n_simulacoes as f32);
+        let promotion : f32 = 100.0 * (*promotion_map.get(team).unwrap() as f32) / (n_simulacoes as f32);
+        let relegation : f32 = 100.0 * (*relegation_map.get(team).unwrap() as f32) / (n_simulacoes as f32);
+        println!("{}\t{:>5.2}\t{:>5.2}\t{:>5.2}",team, title, promotion, relegation);
+    }
 }
 
 
@@ -234,7 +286,7 @@ fn main() {
 fn home_wins() {
     let mut home = crate::Team::create(String::from("VAS"));
     let mut away = crate::Team::create(String::from("GUA"));
-    crate::Team::run_match(&mut home, &mut away, 3, 1);
+    //crate::Team::run_match(&mut home, &mut away, 3, 1);
     assert_eq!(home.goals_for, 3);
     assert_eq!(home.goals_against, 1);
     assert_eq!(home.goal_difference, 2);
@@ -248,7 +300,7 @@ fn home_wins() {
 fn away_wins() {
     let mut home = crate::Team::create(String::from("VAS"));
     let mut away = crate::Team::create(String::from("GUA"));
-    crate::Team::run_match(&mut home, &mut away, 1, 3);
+    //crate::Team::run_match(&mut home, &mut away, 1, 3);
     assert_eq!(away.goals_for, 3);
     assert_eq!(away.goals_against, 1);
     assert_eq!(away.goal_difference, 2);
